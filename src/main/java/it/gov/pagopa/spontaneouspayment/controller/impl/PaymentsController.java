8b74963e9382c6cbecf82794efed4e467d64a215
Jacopo <jacopo.carlini@gmail.com>
2022-07-01 19:51:43 +0200

    fix restcontroller

--- src/main/java/it/gov/pagopa/spontaneouspayment/controller/impl/PaymentsController.java
+++ src/main/java/it/gov/pagopa/spontaneouspayment/controller/impl/PaymentsController.java
@@ -7,9 +7,9 @@ import it.gov.pagopa.spontaneouspayment.service.PaymentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class PaymentsController implements IPaymentsController {
 
     @Autowired
