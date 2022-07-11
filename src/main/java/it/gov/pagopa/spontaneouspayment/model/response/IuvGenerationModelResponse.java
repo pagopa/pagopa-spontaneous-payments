56d31d88fc3f966ef00fd7b78986ea8ffbc258b9
aacitelli <aacitelli@host.docker.internal>
2022-07-11 16:57:07 +0200

    [PPD-238] call iuv generator: added client class

---
+++ src/main/java/it/gov/pagopa/spontaneouspayment/model/response/IuvGenerationModelResponse.java
@@ -0,0 +1,14 @@
+package it.gov.pagopa.spontaneouspayment.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class IuvGenerationModelResponse {
+	private String iuv;
+}
---
+++ src/main/java/it/gov/pagopa/spontaneouspayment/service/IuvGeneratorClient.java
@@ -0,0 +1,25 @@
+package it.gov.pagopa.spontaneouspayment.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import feign.FeignException;
+import it.gov.pagopa.spontaneouspayment.config.FeignConfig;
+import it.gov.pagopa.spontaneouspayment.model.IuvGenerationModel;
+import it.gov.pagopa.spontaneouspayment.model.response.IuvGenerationModelResponse;
+
+
+@FeignClient(value = "iuvgenerator", url = "${service.iuv.generator.host}", configuration = FeignConfig.class)
+public interface IuvGeneratorClient {
+
+    @Retryable(exclude = FeignException.FeignClientException.class, maxAttemptsExpression = "${retry.iuv.generator.maxAttempts}",
+            backoff = @Backoff(delayExpression = "${retry.iuv.generator.maxDelay}"))
+    @GetMapping(value = "/organizations/{organizationfiscalcode}/iuv")
+    IuvGenerationModelResponse generateIUV(@PathVariable("organizationfiscalcode") String organizationFiscalCode,
+                                            @RequestBody IuvGenerationModel paymentPositionModel);
+
+}
