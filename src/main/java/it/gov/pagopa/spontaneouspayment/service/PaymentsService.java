9839934c12b3e0fd2b2c3b05203ce86ca4f77f13
aacitelli <aacitelli@host.docker.internal>
2022-07-12 11:07:56 +0200

    rebase main

--- src/main/java/it/gov/pagopa/spontaneouspayment/service/PaymentsService.java
+++ src/main/java/it/gov/pagopa/spontaneouspayment/service/PaymentsService.java
@@ -1,22 +1,33 @@
 package it.gov.pagopa.spontaneouspayment.service;
 
+import java.time.Year;
+import java.util.Collections;
+import java.util.Optional;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import it.gov.pagopa.spontaneouspayment.entity.Organization;
 import it.gov.pagopa.spontaneouspayment.entity.ServiceProperty;
+import it.gov.pagopa.spontaneouspayment.entity.ServiceRef;
 import it.gov.pagopa.spontaneouspayment.exception.AppError;
 import it.gov.pagopa.spontaneouspayment.exception.AppException;
+import it.gov.pagopa.spontaneouspayment.model.IuvGenerationModel;
 import it.gov.pagopa.spontaneouspayment.model.ServiceModel;
 import it.gov.pagopa.spontaneouspayment.model.SpontaneousPaymentModel;
+import it.gov.pagopa.spontaneouspayment.model.response.IuvGenerationModelResponse;
+import it.gov.pagopa.spontaneouspayment.model.response.PaymentOptionModel;
 import it.gov.pagopa.spontaneouspayment.model.response.PaymentPositionModel;
 import it.gov.pagopa.spontaneouspayment.repository.OrganizationRepository;
 import it.gov.pagopa.spontaneouspayment.repository.ServiceRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Service
 @AllArgsConstructor
@@ -28,11 +39,28 @@ public class PaymentsService {
 
     @Autowired
     private ServiceRepository serviceRepository;
+    
+    @Autowired
+    private GpdClient gpdClient;
+    
+    @Autowired
+    private IuvGeneratorClient iuvGeneratorClient;
+    
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    @Value("${service.aux.digit:3}")
+    private Long auxDigit; 
+    
+    private String iupdPrefix = "GPS" + Year.now().getValue() + "_"; 
+    
+
+    
 
 
     public PaymentPositionModel createSpontaneousPayment(@NotBlank String organizationFiscalCode, @Valid SpontaneousPaymentModel spontaneousPayment) {
         // check if credit institution exists
-        getCreditorInstitution(organizationFiscalCode);
+        var orgConfiguration = getCreditorInstitution(organizationFiscalCode);
 
         // check if service exists
         var serviceConfiguration = getServiceDetails(spontaneousPayment.getService());
@@ -43,7 +71,7 @@ public class PaymentsService {
         // checks if the request contains the properties required by the configured service
         checkServiceProperties(spontaneousPayment, serviceConfiguration);
 
-        return createDebtPosition(serviceConfiguration);
+        return createDebtPosition(organizationFiscalCode, orgConfiguration, serviceConfiguration, spontaneousPayment);
     }
 
 
@@ -65,19 +93,46 @@ public class PaymentsService {
         }
     }
 
-    private PaymentPositionModel createDebtPosition(it.gov.pagopa.spontaneouspayment.entity.Service serviceConfiguration) {
+    private PaymentPositionModel createDebtPosition(String organizationFiscalCode, 
+    		Organization orgConfiguration, it.gov.pagopa.spontaneouspayment.entity.Service serviceConfiguration, SpontaneousPaymentModel spontaneousPayment) {   	
+    	
+    	// call the external service to get the PO
+    	PaymentOptionModel po = this.callExternalService(serviceConfiguration);
+        
+    	// generate IUV
+        String iuv = this.callIuvGeneratorService(organizationFiscalCode, orgConfiguration, serviceConfiguration);
+        
+        // Payment Position to create
+        PaymentPositionModel pp = modelMapper.map(spontaneousPayment.getDebtor(), PaymentPositionModel.class);
+        pp.setIupd(iupdPrefix+iuv);
+        pp.addPaymentOptions(po);
+        
 
-        this.callExternalService(serviceConfiguration);
+        return gpdClient.createDebtPosition(organizationFiscalCode, pp);
+    }
+    
 
-        // TODO: finire di implementare la logica
-        return new PaymentPositionModel();
 
+	private PaymentOptionModel callExternalService(it.gov.pagopa.spontaneouspayment.entity.Service serviceConfiguration) {
+        //TODO implement business logic
+		return new PaymentOptionModel();
     }
 
-    private void callExternalService(it.gov.pagopa.spontaneouspayment.entity.Service serviceConfiguration) {
-        // TODO
+    private String callIuvGeneratorService(String organizationFiscalCode, Organization orgConfiguration, it.gov.pagopa.spontaneouspayment.entity.Service serviceConfiguration) {
+    	
+    	ServiceRef enrollment = Optional.ofNullable(orgConfiguration.getEnrollments()).orElseGet(Collections::emptyList)
+        .parallelStream()
+        .filter(e -> e.getServiceId().equals(serviceConfiguration.getId()))
+        .findAny()
+        .orElseThrow(() -> new AppException(AppError.ENROLLMENT_TO_SERVICE_NOT_FOUND, serviceConfiguration.getId(), organizationFiscalCode));
+    	
+    	Long segregationCode = Long.parseLong(enrollment.getSegregationCode());
+    	IuvGenerationModelResponse iuvObj = iuvGeneratorClient.generateIUV(organizationFiscalCode, 
+    			IuvGenerationModel.builder().auxDigit(auxDigit).segregationCode(segregationCode).build());
+    	
+    	return Optional.ofNullable(iuvObj).orElseThrow(() -> new AppException(AppError.IUV_ACQUISITION_ERROR, organizationFiscalCode, auxDigit, segregationCode)).getIuv();
     }
-
+    
     private void checkServiceOrganization(@NotBlank String organizationFiscalCode,
                                           @NotNull it.gov.pagopa.spontaneouspayment.entity.Service service) {
         var org = orgRepository.getCreditInstitutionByOrgFiscCodeAndServiceId(organizationFiscalCode, service.getId());
@@ -95,6 +150,9 @@ public class PaymentsService {
         return orgRepository.findByFiscalCode(organizationFiscalCode)
                 .orElseThrow(() -> new AppException(AppError.ORGANIZATION_NOT_FOUND, organizationFiscalCode));
     }
+
+
+	
 
 
 }
