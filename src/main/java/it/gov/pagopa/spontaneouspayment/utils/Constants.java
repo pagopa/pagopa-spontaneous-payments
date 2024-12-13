99e169e6859ffdec523b316c9304c02f6b2cfa15
svariant <samuele.varianti@nttdata.com>
2024-12-13 15:02:05 +0100

    fix: Add IUV generator service api key

---
+++ src/main/java/it/gov/pagopa/spontaneouspayment/utils/Constants.java
@@ -0,0 +1,9 @@
+package it.gov.pagopa.spontaneouspayment.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+
+    public static final String APIM_SUBSCRIPTION_KEY = "Ocp-Apim-Subscription-Key";
+}
