9165ed7c2745d9d65c1e2709a2f535c9a9eb62e6
svariant <samuele.varianti@nttdata.com>
2024-12-13 15:02:05 +0100

    fix: Add IUV generator service api key

--- src/main/java/it/gov/pagopa/spontaneouspayment/service/client/IuvGeneratorClient.java
+++ src/main/java/it/gov/pagopa/spontaneouspayment/service/client/IuvGeneratorClient.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.spontaneouspayment.service.client;
 
+import it.gov.pagopa.spontaneouspayment.config.IuvGeneratorConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
@@ -8,12 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import feign.FeignException;
-import it.gov.pagopa.spontaneouspayment.config.FeignConfig;
 import it.gov.pagopa.spontaneouspayment.model.IuvGenerationModel;
 import it.gov.pagopa.spontaneouspayment.model.response.IuvGenerationModelResponse;
 
 
-@FeignClient(value = "iuvgenerator", url = "${service.iuv.generator.host}", configuration = FeignConfig.class)
+@FeignClient(value = "iuvgenerator", url = "${service.iuv.generator.host}",configuration = IuvGeneratorConfig.class)
 public interface IuvGeneratorClient {
 
     @Retryable(exclude = FeignException.FeignClientException.class, maxAttemptsExpression = "${retry.iuv-generator.maxAttempts}",
